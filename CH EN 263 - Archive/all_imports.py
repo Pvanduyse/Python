23a0755cc8fdb64cdec3b750a07914c97f01ceec
Pvanduyse <patricklvanduyse@gmail.com>
2024-09-30 03:27:21 -0600

    Modules moved to a separate repository
    Packaged the modules and pushed to their own repository (myengineerpkg)

--- CH EN 263 - Archive/all_imports.py
+++
@@ -1,7 +0,0 @@
-import numpy             as np
-import matplotlib.pyplot as plt
-import units as u
-from scipy.integrate import quad
-from scipy.optimize import fsolve
-from scipy.optimize import curve_fit
-from scipy.interpolate import interp1d
